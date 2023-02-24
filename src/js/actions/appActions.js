f5720707b995d4434d1f5d49ac8b2c859420394e
Radek Stepan <dev@radekstepan.com>
2023-02-23 20:57:24 -0800

    chore: remove old

--- src/js/actions/appActions.js
+++
@@ -1,4 +0,0 @@
-import EventEmitter from '../lib/EventEmitter.js';
-
-// Just a namespace for all actions.
-export default new EventEmitter();
--- src/js/stores/index.js
+++
@@ -1,8 +0,0 @@
-// The app store needs to go last because it loads user.
-import projectsStore from './projectsStore.js';
-import appStore from './appStore.js';
-
-export default {
-  'app': appStore,
-  'projects': projectsStore
-};
